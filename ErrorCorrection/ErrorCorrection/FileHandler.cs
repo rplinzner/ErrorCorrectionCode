aa0030e816b1971f007111aee298b278e3e117dc
rplinzner <37122411+rplinzner@users.noreply.github.com>
2018-03-20 23:32:44 +0100

    Obsługa plików binarnych

--- ErrorCorrection/ErrorCorrection/FileHandler.cs
+++ ErrorCorrection/ErrorCorrection/FileHandler.cs
@@ -19,10 +19,67 @@ namespace ErrorCorrection
 
             if (ofd.ShowDialog() == true)
             {
-                return File.ReadAllText(ofd.FileName);
+                if (Path.HasExtension(ofd.FileName) == true)
+                {
+                    if (Path.GetExtension(ofd.FileName).Equals(".txt"))
+                    {
+                        return File.ReadAllText(ofd.FileName);
+                    }
+                    if (Path.GetExtension(ofd.FileName).Equals(".bin"))
+                    {
+                        byte[] fileBytes = File.ReadAllBytes(ofd.FileName);
+                        StringBuilder sb = new StringBuilder();
+                        foreach (byte b in fileBytes)
+                        {
+                            sb.Append(Convert.ToString(b, 2).PadLeft(8, '0'));
+                        }
+
+                       var str = sb.ToString();
+                        str = AddNewLineMarkerEveryNChar(str, 16);
+                       return str;
+                    }
+                }
             }
 
             return "";
+        }
+
+
+        public static string RemoveNewLineMarkers(string text)
+        {
+            string outString = string.Empty;
+            foreach (char c in text)
+            {
+                if (c != '\r' && c != '\n')
+                {
+                    outString += c;
+                }
+            }
+
+            return outString;
+        }
+
+        public static string AddNewLineMarkerEveryNChar(string text, int n)
+        {
+            int lines = text.Length / n;
+            string output = string.Empty;
+            for (int i = 0; i < lines; i++)
+            {
+                for (int j = 0; j < n+1; j++)
+                {
+                    if (j.Equals(n))
+                    {
+                        output += '\r';
+                        output += '\n';
+                    }
+                    else
+                    {
+                        output += text[(i * n) + j];
+                    }
+                }
+            }
+
+            return output;
         }
 
         public static bool SaveFile(string text, string mode) //metoda sluzaca do zapisu do pliku
@@ -30,17 +87,37 @@ namespace ErrorCorrection
             SaveFileDialog sfd = new SaveFileDialog();
             if (mode.Contains("TXT")) sfd.Filter = "Plik Tekstowy (*.txt)|*.txt"; //opcja zapisu do pliku txt
             else if (mode.Contains("BIN")) sfd.Filter = "Plik Binarny (*.bin)|*.bin"; //opcja zapisu do pliku bin
-            else sfd.Filter = "Plik Dowolny (*.*)|*.*";
             sfd.FileName = "Wiadomosc";
+            
             if (sfd.ShowDialog() == true) 
             {
-                File.WriteAllText(sfd.FileName, text);
-                return true;
+
+                if (Path.GetExtension(sfd.FileName).Equals(".txt"))
+                {
+                    File.WriteAllText(sfd.FileName, text);
+                    return true;
+                }
+
+                if (Path.GetExtension(sfd.FileName).Equals(".bin"))
+                {
+                    string textNoNewLine = RemoveNewLineMarkers(text);
+                    int numOfBytes = textNoNewLine.Length / 8;
+                    byte[] bytes = new byte[numOfBytes];
+                    for (int i = 0; i < numOfBytes; ++i)
+                    {
+                        bytes[i] = Convert.ToByte(textNoNewLine.Substring(8 * i, 8), 2);
+                    }
+                    File.WriteAllBytes(sfd.FileName,bytes);
+                    return true;
+                }
+                
+                
             }
 
             return false;
         }
 
+      
         public static string print_array_n_row(char[,] table, int rows) //metoda sluzaca do wypisania macierzy w oknie dialogowym
         {
             String temp = String.Empty; //deklaracja pustego stringa
